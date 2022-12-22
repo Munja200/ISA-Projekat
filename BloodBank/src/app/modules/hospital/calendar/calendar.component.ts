89d21fdbd6c3cf7ea89ff5434f83079746829a29
galicc <ivan500galich@gmail.com>
2022-12-22 12:59:22 +0100

    Calendar Implementation Front

--- BloodBank/src/app/modules/hospital/calendar/calendar.component.ts
+++ BloodBank/src/app/modules/hospital/calendar/calendar.component.ts
@@ -1,16 +1,111 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, ViewChild, AfterViewInit} from "@angular/core";
+import {
+  DayPilot,
+  DayPilotCalendarComponent,
+  DayPilotMonthComponent,
+  DayPilotNavigatorComponent
+} from "@daypilot/daypilot-lite-angular";
 
 @Component({
-  selector: 'app-calendar',
+  selector: 'calendar-component',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements AfterViewInit {
 
-  constructor(private router: Router) { }
+  @ViewChild("day") day!: DayPilotCalendarComponent;
+  @ViewChild("week") week!: DayPilotCalendarComponent;
+  @ViewChild("month") month!: DayPilotMonthComponent;
+  @ViewChild("navigator") nav!: DayPilotNavigatorComponent;
 
-  ngOnInit(): void {
+  events: DayPilot.EventData[] = [];
 
+  date = DayPilot.Date.today();
+
+  configNavigator: DayPilot.NavigatorConfig = {
+    showMonths: 3,
+    cellWidth: 25,
+    cellHeight: 25,
+    onVisibleRangeChanged: args => {
+      this.loadEvents();
+    }
+  };
+
+  selectTomorrow() {
+    this.date = DayPilot.Date.today().addDays(1);
   }
+
+  changeDate(date: DayPilot.Date): void {
+    this.configDay.startDate = date;
+    this.configWeek.startDate = date;
+    this.configMonth.startDate = date;
+  }
+
+  configDay: DayPilot.CalendarConfig = {
+  };
+
+  configWeek: DayPilot.CalendarConfig = {
+    viewType: "Week",
+    onTimeRangeSelected: async (args) => {
+      const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
+      const dp = args.control;
+      dp.clearSelection();
+      if (!modal.result) { return; }
+      dp.events.add(new DayPilot.Event({
+        start: args.start,
+        end: args.end,
+        id: DayPilot.guid(),
+        text: modal.result
+      }));
+    }
+  };
+
+  configMonth: DayPilot.MonthConfig = {
+
+  };
+
+  constructor() {
+    this.viewWeek();
+  }
+
+  ngAfterViewInit(): void {
+    this.loadEvents();
+  }
+
+  loadEvents(): void {
+    const from = this.nav.control.visibleStart();
+    const to = this.nav.control.visibleEnd();
+    this.events = [
+      {
+        id: "1",
+        start: DayPilot.Date.today().addHours(10),
+        end: DayPilot.Date.today().addHours(12),
+        text: "Event 1"
+      }
+    ];
+  }
+
+  viewDay():void {
+    this.configNavigator.selectMode = "Day";
+    this.configDay.visible = true;
+    this.configWeek.visible = false;
+    this.configMonth.visible = false;
+  }
+
+  viewWeek():void {
+    this.configNavigator.selectMode = "Week";
+    this.configDay.visible = false;
+    this.configWeek.visible = true;
+    this.configMonth.visible = false;
+  }
+
+  viewMonth():void {
+    this.configNavigator.selectMode = "Month";
+    this.configDay.visible = false;
+    this.configWeek.visible = false;
+    this.configMonth.visible = true;
+  }
+
+
 }
+
