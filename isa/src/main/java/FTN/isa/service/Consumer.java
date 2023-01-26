440f7c7397299e595c4bf7f9b61406f22f375fe4
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2023-01-26 11:31:45 +0100

    Added consumer on isa side

---
+++ isa/src/main/java/FTN/isa/service/Consumer.java
@@ -0,0 +1,18 @@
+package FTN.isa.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+	private static final Logger log = LoggerFactory.getLogger(Consumer.class);
+
+	
+	@RabbitListener(queues="${myqueue2}")
+	public void handler(String message) {
+		log.info("Consumer2> " + message);
+	}
+}
