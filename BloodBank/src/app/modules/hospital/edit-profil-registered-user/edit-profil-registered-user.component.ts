76cbb904455061cd5a3882d639ab3535a72e4974
Dejana Tesanovic <dejana.tesanovic5@gmail.com>
2023-06-13 02:53:37 +0200

    Fixed navbar and edit profile

--- BloodBank/src/app/modules/hospital/edit-profil-registered-user/edit-profil-registered-user.component.ts
+++ BloodBank/src/app/modules/hospital/edit-profil-registered-user/edit-profil-registered-user.component.ts
@@ -5,6 +5,9 @@ import { Person } from '../model/person';
 import { RegisteredPerson } from '../model/registeredPerson';
 import { RegisteredUserUpdateDTO } from '../model/registeredUserUpdateDTO';
 import { RegisterPersonService } from '../services/register-person.service';
+import { FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-edit-profil-registered-user',
@@ -12,27 +15,92 @@ import { RegisterPersonService } from '../services/register-person.service';
   styleUrls: ['./edit-profil-registered-user.component.css']
 })
 export class EditProfilRegisteredUserComponent implements OnInit {
-  
-  public address: Address = new Address(0, false, 0, 0, '', '', '', '');
+   
+  public dataSource = new MatTableDataSource<Person>();
+
+  public address: Address = new Address();
   public person: Person = new Person(0,'','','','','','','',0,new Date(),'','','',this.address);
   public registeredUser: RegisteredPerson = new RegisteredPerson(0, false, this.person);
-  public registeredUserDto: RegisteredUserUpdateDTO = new RegisteredUserUpdateDTO(4, '', '', '', '', '', '', '', new Date(), '', this.address, '');
+  public id : number = 0;
+  public personId : number = 0;
+  public registeredUserDto: RegisteredUserUpdateDTO = new RegisteredUserUpdateDTO(0, '', '', '', '', '', '', '', new Date(), '', this.address, '');
+  
 
-  constructor(private registerPersonService: RegisterPersonService,private router: Router) { }
+  constructor(private authService: AuthService, private registerPersonService: RegisterPersonService, private router: Router) { }
 
   ngOnInit(): void {
-    this.registerPersonService.getRegisteredPerson(4).subscribe(res => {
-      this.registeredUser = res;
-      console.log(this.registeredUser);
-  })
-}
+    const username = this.authService.getCurrentUserUsername();
+    console.log(username);
+    if (username) {
+      this.registerPersonService.getUserByUsername(username).subscribe(osoba => {
+          this.registeredUserDto = osoba;
+          this.registeredUserDto.password = '';
+      });
+    }
+  }
 
   public editProfile() {
-    this.registerPersonService.updateRegisteredUser(this.registeredUserDto).subscribe(res => {
-      this.registeredUser = res;
-      window.confirm("Information of registered user is changed!");
+    if (this.isInputValid()) {
+      console.log(this.person);
+      this.registerPersonService.updateRegisteredUser(this.registeredUserDto).subscribe(res => {
+        this.registeredUserDto = res;      
+        alert("You have successfully updated!");
+        this.router.navigate(['/home']);
+      });
+    } else {
+      alert("You must fill in all fields!");
+    }
+  }
 
-  })
+  private isInputValid(): boolean {
+    return this.registeredUserDto.name != '' && this.registeredUserDto.surname != '' && this.registeredUserDto.phonNumber != '' && this.registeredUserDto.address.country != '' && this.registeredUserDto.address.city != ''
+    && this.registeredUserDto.address.street != '' && this.registeredUserDto.occupation != '' && this.registeredUserDto.bloodType != '' && this.registeredUserDto.password != ''
 }
+
+  logout() {
+    
+  }
+
+  requiredBloodTypeControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(A|B|AB|O)[+-]$/)
+  ]);
+  requiredPasswordControl = new FormControl('', [
+    Validators.required,
+  ]);
+  requiredNameControl = new FormControl('', [
+    Validators.required,
+  ]);
+  requiredNumberControl = new FormControl('', [
+    Validators.required,
+  ]);
+  requiredSurnameControl = new FormControl('', [
+    Validators.required,
+  ]);
+  requiredJmbgControl = new FormControl('', [
+    Validators.required,
+  ]);
+  requiredConfirmationPasswordControl = new FormControl('', [
+    Validators.required,
+  ])
+  requiredPhoneNumberControl = new FormControl('', [
+    Validators.required,
+  ]);
+  requiredOccupationControl = new FormControl('', [
+    Validators.required,
+  ]);
+  requiredAddressControl = new FormControl('', [
+    Validators.required,
+  ]);
+  requiredCityControl = new FormControl('', [
+    Validators.required,
+  ]);
+  requiredCountryControl = new FormControl('', [
+    Validators.required,
+  ]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
 }
